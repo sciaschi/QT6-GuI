b5bd6595f7efbba74d3271a53a899f57a9eab5e9
Sean Ciaschi <sciaschi1@gmail.com>
2025-02-19 16:06:55 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,28 +1,15 @@
 #include <QApplication>
 #include <QLabel>
 #include <QTimer>
-#include <csignal> // For signal handling
-
-// Global pointer to QApplication for signal handling
-QApplication *appPtr = nullptr;
-
-// Signal handler function
-void handleSignal(int signal) {
-    if (signal == SIGINT && appPtr) {
-        appPtr->quit();
-    }
-}
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-    appPtr = &app; // Store the QApplication pointer
 
-    // Set up the signal handler
-    std::signal(SIGINT, handleSignal);
-
-    QLabel label("Press Ctrl+C to exit");
+    QLabel label("Exiting in 5 seconds...");
     label.resize(200, 100);
     label.show();
+
+    QTimer::singleShot(5000, &app, &QApplication::quit); // Exit after 5 seconds
 
     return app.exec();
 }
